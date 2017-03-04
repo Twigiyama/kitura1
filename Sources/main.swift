799c4cc4df9daabc17f0a136501f858c2caddd6a
Twigiyama <asitha@tomorrows.company>
2017-03-04 13:41:06 +0000

    Created three different routes

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,6 +7,23 @@ import HeliumLogger
 HeliumLogger.use(.info)
 let router = Router()
 
+router.get("/") {
+    request, response, next in
+    response.send("Welcome to million hairs!")
+    next()
+}
+
+router.get("/staff") {
+    request, response, next in
+    response.send("Meet our great team")
+    next()
+}
+
+router.get("/contact") {
+    request, response, next in
+    response.send("Get in touch with us")
+}
+
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Log.info("Haters gonna hate anyway")
 Kitura.run()
